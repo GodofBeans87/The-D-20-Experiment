3a6980e50ed2b27276ea1b8d4de041b5a63ee458
GodofBeans87 <146485821+GodofBeans87@users.noreply.github.com>
2023-10-16 15:38:26 -0500

    Add files via upload

---
+++ Randomd20.py
@@ -0,0 +1,13 @@
+
+
+import random
+randomDice = random.randrange(1,20)
+print(randomDice)
+if randomDice==(20):
+    print ('crit hit')
+if randomDice==(1):
+    print ('haha')
+if randomDice==(10):
+    print ('dead center')
+if randomDice==(18) :
+    print ('Iamgalactusdestroyerofworlds')
